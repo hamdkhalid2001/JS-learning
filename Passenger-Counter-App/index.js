6c8612d5257e02edc73542baeddca16e4d5b365b
Hamd Khalid <hamdbinkhalid@gmail.com>
2022-12-21 22:17:51 +0500

    Implemented save feature

--- Passenger-Counter-App/index.js
+++ Passenger-Counter-App/index.js
@@ -1,6 +1,13 @@
 let count = 0
 let totalPeople = document.getElementById('total-people')
+let prevEntries = document.getElementById('entries')
 function increment(){
     count = count + 1
-    totalPeople.innerText = count ;
+    totalPeople.innerText = count;
+}
+
+function save(){
+    prevEntries.textContent +=   count + " - ";
+    count = 0
+    totalPeople.innerText = count;
 }
