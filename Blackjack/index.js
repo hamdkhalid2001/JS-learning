bbf5ec9f4569547d85d736c4f429b9377fe9f690
Hamd Khalid <hamdbinkhalid@gmail.com>
2022-12-22 20:06:52 +0500

    Add new card btn

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,11 +1,39 @@
 let firstCard = 11
-let secondCard = 11 
+let secondCard = 2
+
 let sum = firstCard + secondCard
 
-if(sum <= 20){
-    console.log("Do you want to draw another card?")
-}else if(sum === 21){
-    console.log("You've got a Blackjack")
-}else{
-    console.log("You lose")
+let hasBlackjack = false
+let isAlive = true
+let message = ""
+
+let messageEl = document.getElementById("message-heading")
+messageEl.textContent = 'Want to play a round?'
+
+let sumEl = document.querySelector("#sumEl")
+let cardsEl = document.querySelector(".cardsEl")
+
+function startGame(){
+    sumEl.textContent = `Sum: ${sum}`
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard 
+    if(sum <= 20){
+        console.log("Do you want to draw another card?")
+        message = "Do you want to draw another card?"
+    }else if(sum === 21){
+        console.log("You've got a Blackjack")
+        message = "You've got a Blackjack"
+        hasBlackjack = true
+    }else{
+        isAlive = false
+        message = "You lose"
+    }
+    messageEl.textContent = message
 }
+
+function newCard(){
+    let newCard = 12
+    sum += newCard
+    startGame()
+}
+
+console.log(message)
