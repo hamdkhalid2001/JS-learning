f2e0daeb2f7977f333b0de6c27d4b2aa57fba165
Hamd Khalid <hamdbinkhalid@gmail.com>
2022-12-23 21:54:10 +0500

    Add: Local Storage

--- Links_Saver/index.js
+++ Links_Saver/index.js
@@ -1,19 +1,23 @@
-const myLink = []
+let myLink = []
 const inputEl = document.getElementById("input-el")
 const buttonEl = document.querySelector(".btn")
 const listEl = document.querySelector("#links-list")
 
+renderLinks()
+
 buttonEl.addEventListener("click",()=>{
     if(!inputEl.value) return
     myLink.push(inputEl.value)
+    localStorage.setItem("myLinks",JSON.stringify(myLink))
     renderLinks()
     inputEl.value = ""
 })
 
 function renderLinks(){
-    // const newLink = document.createElement('li');
-    // newLink.innerText = item
-    // listEl.insertAdjacentElement("beforeend",newLink)
-    listEl.innerHTML += `<a href="${inputEl.value}" target="_blank"><li>${inputEl.value}</li></a>`
+    myLink = JSON.parse(localStorage.getItem("myLinks"))
+    listEl.innerHTML = ""
+    myLink.forEach((Element)=>{
+        listEl.innerHTML += `<a href="${Element}" target="_blank"><li>${Element}</li></a>`
+    })
     
 }
